6f056ae73bf5e4127c241d0fa907fdefb4559e62
Anara <alibek020410@gmail.com>
2020-09-07 22:25:38 +0900

    HelloWorld start

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -45,7 +45,7 @@ public class Animal{
     }
 
     public String toStringFull(){
-          return  "I am " + getKind() +", eat " + getFoodKind() + calculateFoodWeight();
+          return  "I am " + getKind() +", eat " + getFoodKind() +" "+ calculateFoodWeight();
     }
     public static void main(String[] args) {
 

5402412cab792bf4781282ec4110a0b7eaa9414e
PC <alibek020410@gmail.com>
2020-10-27 20:00:43 +0900

    SSS

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -2,26 +2,26 @@ package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
     public static final double PI = 3.14;
-    public static final double EARTH_RADIUS = 6371.2;
+    public static final double R = 6371.2;
 
 
     public static double volumeBallDouble(double radius){
-        final double VOLUME_BALL = 4.0 / 3.0 * PI * (EARTH_RADIUS * EARTH_RADIUS * EARTH_RADIUS);
-        return VOLUME_BALL;
+        final double VOLUME = 4.0 / 3.0 * PI * (radius * radius * radius);
+        return VOLUME;
     }
     public static float volumeBallFloat(float radius){
-        final double VOLUME_BALL = 4.0 / 3.0 * PI * (EARTH_RADIUS * EARTH_RADIUS * EARTH_RADIUS);
-        return (float) VOLUME_BALL;
+        final double VOLUME = 4.0 / 3.0 * PI * (radius * radius * radius);
+        return (float) VOLUME;
     }
     public static double calculateAccuracy(double radius){
-        return volumeBallDouble(0)-volumeBallFloat(0);
+        return volumeBallDouble(radius)-volumeBallFloat((float) radius);
     }
 
     public static void main(String[] args) {
+        calculateAccuracy(R);
+        System.out.println(volumeBallDouble(R));
+        System.out.println(volumeBallFloat((float)R));
 
-        System.out.println(volumeBallDouble(EARTH_RADIUS));
-        System.out.println(volumeBallFloat((float)EARTH_RADIUS));
     }
 }
-
 

3ef4ab44fb2f16a6b1a1b69bba58aca2428d49cf
PC <alibek020410@gmail.com>
2020-11-15 19:40:02 +0900

    SSS

---
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -0,0 +1,55 @@
+package ru.progwards.java1.lessons.classes;
+
+public class Animal {
+    double weight;
+
+    public Animal(double weight){
+    this.weight = weight;
+    }
+
+    enum AnimalKind{
+        ANIMAL,
+        COW,
+        HAMSTER,
+        DUCK,
+        ;
+
+
+    }
+    enum FoodKind{
+        UNKNOWN,
+        HAY,
+        CORN,
+    }
+
+    public AnimalKind getKind(){
+        return AnimalKind.ANIMAL;
+    }
+    public FoodKind getFoodKind(){
+        return FoodKind.UNKNOWN;
+    }
+    public String toString(){
+        return "I am" +  getKind() +"," + "eat" + getFoodKind();
+    }
+    public double getWeight(){
+        return weight;
+    }
+    public double getFoodCoeff(){
+        return 0.02;
+    }
+    public double calculateFoodWeight(){
+        return getWeight()*getFoodCoeff();
+    }
+    public String toStringFull(){
+        return "I am" + getKind() + "," + "eat" + getFoodKind() + calculateFoodWeight();
+    }
+    public static void main(String[] args) {
+
+        System.out.println(" I am  " + AnimalKind.ANIMAL +", eat " + FoodKind.UNKNOWN);
+        System.out.println(" I am  " + AnimalKind.COW +", eat " + FoodKind.HAY);
+        System.out.println(" I am  " + AnimalKind.HAMSTER +", eat " + FoodKind.CORN);
+        System.out.println(" I am  " + AnimalKind.DUCK +", eat " + FoodKind.CORN);
+    }
+}
+
+
---
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -0,0 +1,42 @@
+package ru.progwards.java1.lessons.classes;
+
+public class ComplexNum {
+    int a;
+    int b;
+
+    public ComplexNum(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public String toString(){
+        String result = String.valueOf(a) + b + "i";
+        return result;
+    }
+    public ComplexNum add(ComplexNum num){
+        ComplexNum result = new ComplexNum(this.a + num.a,this.b + num.b);
+        return result;
+    }
+    public ComplexNum sub(ComplexNum num){
+        ComplexNum result = new ComplexNum(this.a - num.a,this.b - num.b);
+        return result;
+    }
+    public ComplexNum mul(ComplexNum num){
+        ComplexNum result = new ComplexNum((this.a * num.a - this.b * num.b),(this.b * num.a + this.a * num.b));
+        return result;
+    }
+    public ComplexNum div(ComplexNum num){
+        ComplexNum result = new ComplexNum((this.a * num.a + this.b * num.b) / (num.a * num.a +num.b * num.b),(this.b * num.a -this.a * num.b) / (num.a * num.a + num.b * num.b));
+         return result;
+    }
+
+    public static void main(String[] args) {
+        ComplexNum result1 = new ComplexNum(1,56);
+        ComplexNum result2 = new ComplexNum(5,-1);
+
+        System.out.println("сложение комплексных чисел " + result1.add(result2));
+        System.out.println("вычитание комплексных чисел " + result1.sub(result2));
+        System.out.println("умножение комплексных чисел " + result1.mul(result2));
+        System.out.println("деление комплексных чисел " + result1.div(result2));
+    }
+    }
+
